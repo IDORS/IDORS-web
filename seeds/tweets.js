3a73a687ce0ff1e62b16af31bc6e570d6c549571
manuel-saravia <manuel.saravia@fing.edu.uy>
2019-08-06 20:26:43 -0300

    Modified the whole tweets seeding file

--- seeds/tweets.js
+++ seeds/tweets.js
@@ -1,58 +1,67 @@
 const fs = require('fs');
+const tweetsPerType = 12500;
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const readFileAndInsert = async function(file, knex) {
+const readFileAndGetJSON = function(file) {
   const text = fs.readFileSync(file, { encoding: 'utf-8' });
-  const tweetPromises = [];
-  var count = 0;
+  var tweets = [];
   text.split('\n').forEach((line) => {
-    if (line.length == 0) { 
-      console.log(file, 'aca', count); 
-      return;
-    }
-    count++;
+    if (line.length == 0) return;
     const tweet = JSON.parse(line);
     const {id, user, text} = tweet;
-    tweetPromises.push(createRecord(knex, {id, user, text}));
-    tweetPromises.push(sleep(2000));
+    tweets.push({id, user, text});
   });
-  
-  return Promise.all(tweetPromises)
+
+  return tweets;
 };
 
-const createRecord = async function (knex, data) {
-  return await knex('tweets')
-          .insert({
-            id   : data.id,
-            user : data.user,
-            text : data.text,
-          })
-          .catch((error) => {
-            if (error.code != 'ER_DUP_ENTRY') {
-              return createRecord(knex, data);
-            } else {
-              console.log(data.id, erroc.code);
-            }
-          })
+const createRecords = async function (knex, data) {
+  return await knex.raw(
+                knex('tweets')
+                  .insert(data)
+                  .toString()
+                  .replace('insert', 'INSERT IGNORE'))
+              .catch((error) => {
+                if (error.code == 'ER_LOCK_DEADLOCK') return createRecords(knex, data);
+                else console.log(error.code);
+              });
 };
 
-exports.seed = function(knex) {
+exports.seed = async function(knex) {
   return knex('tweets')
           .then(() => {
             const folders = fs.readdirSync('tweets');
-            const insertedTweets = [];
+            const insertionPromises = [];
+            const includedTweets = new Set([]);
 
             folders.forEach((folder) => {
+              const tweetsInFolder = []; //Tweets from the folder that will be inserted
+              const tweetsInEachFile = []; //Tweets in each file of the folder
+              var totalInFolder = 0; //Total number of tweets in this folder
               const tweetFiles = fs.readdirSync('tweets/' + folder + '/');
 
               tweetFiles.forEach((fileName) => {
-                insertedTweets.push(readFileAndInsert('tweets/' + folder + '/' + fileName, knex));
+                const tweetsInFile = readFileAndGetJSON('tweets/' + folder + '/' + fileName);
+                tweetsInEachFile.push(tweetsInFile);
+                totalInFolder += tweetsInFile.length;
               });
+
+              tweetsInEachFile.forEach((tweets) => {
+                const neededLength = tweetsPerType * ( tweets.length/totalInFolder );
+                var actualLength = 0;
+
+                while (actualLength < neededLength && tweets.length != 0) {
+                  var removedIndex = Math.floor( Math.random()*tweets.length );
+                  const tweet = tweets.splice(removedIndex, 1)[0];
+                  if (!includedTweets.has(tweet.id)) {
+                    includedTweets.add(tweet.id);
+                    tweetsInFolder.push(tweet);
+                    actualLength++;
+                  }
+                }
+              });
+              insertionPromises.push(createRecords(knex, tweetsInFolder));
             });
-            
-            return Promise.all(insertedTweets)
+
+            return Promise.all(insertionPromises);
           });
 };
